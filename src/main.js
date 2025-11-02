ad65018c409ba5d4e88bb67da9a76396db04b0cf
Dylan DiBeneditto <dibenedittod@gmail.com>
2025-11-02 00:06:38 -0400

    setup

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-(async () => {})();
+import { Application } from "pixi.js";
+
+(async () => {
+    const app = new Application();
+
+    await app.init({
+        resizeTo: window,
+    })
+
+    document.body.appendChild(app.canvas);
+})();
