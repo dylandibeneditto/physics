b30684186f389c0425b9c034118a6c94d6ff246e
Dylan DiBeneditto <dibenedittod@gmail.com>
2025-12-16 21:16:08 -0500

    reset

--- main.js
+++
@@ -1,7 +0,0 @@
-import './style.css'
-import Experience from './experience/experience'
-import { initDevtools } from '@pixi/devtools'
-
-const experience = new Experience(document.getElementById("canvas"))
-
-initDevtools({experience.app});
