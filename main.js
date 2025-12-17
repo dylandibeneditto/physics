86d86fbf09679738393c9ff05d986871ae7432a9
Dylan DiBeneditto <dibenedittod@gmail.com>
2025-12-17 00:42:10 -0500

    built out webpage

---
+++ main.js
@@ -0,0 +1,6 @@
+import Anchor from "./objects/anchor.js"
+
+const canvas = document.getElementById("canvas");
+const c = canvas.getContext("2d")
+
+c.fillStyle("black")
---
+++ objects/anchor.js
@@ -0,0 +1,13 @@
+export default class Anchor {
+    constructor(x, y, w, h, linewidth) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.linewidth = linewidth
+    }
+
+    render() {
+        
+    }
+}
